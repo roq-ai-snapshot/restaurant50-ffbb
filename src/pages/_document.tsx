234db47848c4cee36e8dd4238299fea5671ddf61
roq-ai-snapshot <fabian.wesner@roq.tech>
2023-06-28 11:28:42 +0000

    Initial commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* start tracking-scripts */}
+        {/* end tracking-scripts */}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
